2190bb2f576a4035cbbe2f36d8e5a54a624bb676
Thales Rodrigues <thaleshend@gmail.com>
2023-08-27 11:08:38 -0300

    v0.0

---
+++ display.py
@@ -0,0 +1,64 @@
+import pygame
+from math import atan2, radians
+from threading import Thread
+
+from Vector import Vector2, Vector3
+
+
+class Display:
+    def __init__(self) -> None:
+        self.screen_size = Vector2(600, 500)
+
+        self.fov = radians(45)
+        self.cam_pos = Vector3(0, -1.5, .5)
+
+        self.vertices = []
+
+        self.thread = Thread(target=self.loop)
+
+        self.RUNNING = True
+
+    def start(self):
+        self.thread.start()
+
+    def project(self, point_pos):
+        point_dist_hor = (point_pos * Vector3(1, 1)).magnitude()
+
+        point_angle_vert = atan2(point_pos.z, point_dist_hor)
+        point_angle_hor = atan2(point_pos.x, point_pos.y)
+
+        return (self.screen_size * 0.5) * Vector2(point_angle_hor/self.fov + 1, point_angle_vert/self.fov + 1)
+
+    def update(self, matrix):
+        # vertices[0] = (color, pos_2d)
+        
+        #vertices = [(tuple(Vector3(255, 255, 255) * (1 - p.magnitude() / max_dist)), p - self.cam_pos) for l in matrix for p in l]
+        #vertices = [(tuple(Vector3(255, 255, 255) * max(0, min(1, 1-abs(p.z)))), p - self.cam_pos) for l in matrix for p in l]
+        #vertices = [(tuple(Vector3(255, 255, 255) * (1 - (p - self.cam_pos).magnitude() / max_dist)), p - self.cam_pos) for l in matrix for p in l]
+        vertices = [((255, 255, 255), p - self.cam_pos) for l in matrix for p in l]
+
+        vertices.sort(key=lambda x: x[1].magnitude(), reverse=True)
+
+        vertices = [(i[0], self.project(i[1])) for i in vertices]
+
+        self.vertices = vertices
+
+    def end(self):
+        self.RUNNING = False
+
+    def loop(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(tuple(self.screen_size))
+        pygame.display.set_caption('Surface Lidar')
+
+        while self.RUNNING:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.RUNNING = False
+                
+            self.screen.fill((52, 53, 65)) # Clear Screen
+
+            for (color, pos) in self.vertices:
+                pygame.draw.circle(self.screen, color, tuple(pos), 2)
+
+            pygame.display.update() # Update Screen
---
+++ main.py
@@ -0,0 +1,121 @@
+from time import sleep
+import krpc
+
+from display import Display
+
+from Vector import Vector2, Vector3
+
+class Ray:
+    def __init__(self, x, y) -> None:
+        self.direction = Vector3(-focal_length, x, y).normalize()
+        self.distance = float('inf')
+        self.pos = Vector3(x, y, self.distance) # direction nao esta no mesmo referencial
+
+    def update(self):
+        self.distance = space_center.raycast_distance((0, 0, 0), tuple(self.direction), lidar_ref)
+
+        self.pos.z = -self.direction.x * self.distance
+
+
+conn = krpc.connect(name='Hover')
+drawing = conn.drawing
+space_center = conn.space_center
+vessel = space_center.active_vessel
+body = vessel.orbit.body
+a_gravity = body.surface_gravity
+
+body_ref = body.reference_frame
+surface_ref = vessel.surface_reference_frame
+hybrid_ref = space_center.ReferenceFrame.create_hybrid(
+    position=body_ref,
+    rotation=surface_ref
+)
+
+flight_body = vessel.flight(body_ref)
+flight_hybrid = vessel.flight(hybrid_ref)
+
+stream_mean_altitude = conn.add_stream(getattr, flight_body, "mean_altitude")
+stream_vel = conn.add_stream(getattr, flight_hybrid, "velocity")
+stream_mass = conn.add_stream(getattr, vessel, "mass")
+stream_av_thrust = conn.add_stream(getattr, vessel, "available_thrust")
+
+# PARAMS
+matrix_size = [10, 10]
+focal_length = 5
+alt_target = stream_mean_altitude()
+
+
+# Lidar
+lidar_ref =space_center.ReferenceFrame.create_hybrid(
+    position=space_center.ReferenceFrame.create_relative(reference_frame=vessel.reference_frame, position=(0, vessel.bounding_box(vessel.reference_frame)[0][1] - 1, 0)),
+    rotation=surface_ref
+)
+
+'''
+lidar_ref = space_center.ReferenceFrame.create_relative(
+    reference_frame=surface_ref,
+    position=(-5, 0, 0)
+)
+'''
+
+display = Display()
+display.start()
+
+points = []
+
+for j in range(matrix_size[1]):
+    row = []
+    y = j / (matrix_size[1] - 1) - 0.5
+    for i in range(matrix_size[0]):
+        x = i / (matrix_size[0] - 1) - 0.5
+        ray = Ray(x, y)
+        #drawing.add_line((0, 0, 0), tuple(ray.direction * 500), lidar_ref)
+        row.append(ray)
+    points.append(row)
+
+try:
+    while True:
+        sleep(0.05)
+
+        display_points = []
+        for row in points:
+            line = []
+            for ray in row:
+                ray.update()
+
+                line.append(ray.pos)
+
+            display_points.append(line)
+
+        
+        max_z = max([r.pos.z for l in points for r in l])
+
+        for row in display_points:
+            for point in row:
+                point.z /= max_z
+
+        display.update(display_points)
+
+
+
+
+    
+        # Throttle control
+        alt = stream_mean_altitude()
+        vel = Vector3(stream_vel())
+        mass = stream_mass()
+        av_thrust = stream_av_thrust()
+
+
+        a_eng = av_thrust / mass
+
+        delta_h = alt_target - alt
+
+        v_target = delta_h - (a_eng - a_gravity) *.5
+
+        delta_v = v_target - vel.x
+
+        vessel.control.throttle = (delta_v*5 + a_gravity) / a_eng
+
+except KeyboardInterrupt as e:
+    display.end()
